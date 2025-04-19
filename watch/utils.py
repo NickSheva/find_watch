d4aaaa3ffb0dc1b0c308978beaa290f3cb005e9d
NickSheva <nikshevtsov@mail.ru>
2025-04-19 18:32:27 +0500

    🐛 Fix: синтаксис launch_args (замена = на :)

--- watch/utils.py
+++ watch/utils.py
@@ -87,7 +87,7 @@ async def parse_products_page(page_num: int, items_limit: int = None) -> list:
     async with async_playwright() as p:
         launch_args = {
             "headless": HEADLESS,
-            "args"=[
+            "args": [
                 "--disable-dev-shm-usage",  # уже есть, но это важно
                 "--disable-background-networking",
                 "--disable-background-timer-throttling",
